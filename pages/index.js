bcd6c775773a5063fd5ed19eb6e079592496d7d1
Rich Bakos <rbakos@gmail.com>
2025-06-30 07:08:23 -0600

    Refactor home page content for improved structure and readability

--- pages/index.js
+++ pages/index.js
@@ -18,78 +18,83 @@ export default function Home({ tools }) {
             {/* Left: Text centered in its own div */}
             <div className="md:w-[60%] w-full flex text-center">
                 <div className="w-full mt-[5%]">
-                    <h1 className="text-headingWhite text-3xl md:text-4xl font-bold mb-4">
-                        Welcome to The AI Tool Pouch
-                    </h1>
-                    <p className="text-grayText">
-                        There is an endless sea of AI driven tools in the market
-                        today.
-                    </p>
-                    <p className="text-grayText">
-                        Are you new to AI? Maybe you have a project with
-                        increased productivity goals?
-                    </p>
-                    <p className="text-grayText">
-                        You don't have to fumble through countless feature lists
-                        to find a solution.
-                    </p>
-                    <p className="text-grayText">
-                        We'll present information in a way that enables quick,
-                        well informed choices.
-                    </p>
-                    <h1 className="text-headingWhite text-xl md:text-xl font-bold mt-4 mb-4">
-                        Discover powerful AI tools tailored to your profession.
-                    </h1>
-                    <p className="text-grayText">
-                        We'll show you a manageable list of tools to choose
-                        from.
-                    </p>
-                    <p className="text-grayText">
-                        We'll show you detailed side by side comparisons.
-                    </p>
-                    <p className="text-grayText">
-                        We'll show you <strong>who</strong> each one is for.
-                    </p>
-                    <p className="text-grayText">
-                        We'll show you <strong>what</strong> each one does.
-                    </p>
-                    <p className="text-grayText">
-                        We'll show you <strong>why</strong> each one should
-                        matter.
-                    </p>
-                    <p className="text-grayText">
-                        We'll link back to he vendor so you know{" "}
-                        <strong>where</strong> to get each one.
-                    </p>
+                    <div className="max-w-2xl mx-auto text-center">
 
-                    <p className="text-grayText mt-4 mb-4">
-                        <strong>- so act now -</strong>
-                    </p>
 
-                    <p className="text-grayText">
-                        Stop fumbling through feature lists.
-                    </p>
-                    <p className="text-grayText">
-                        You can quickly determine which tools belong in your
-                        tool pouch.
-                    </p>
-                    <p className="text-grayText mb-4">
-                        Select from one of our
-                        <Link
-                            href="/categories"
-                            className="text-accentGreen hover:text-headingWhite transition"
-                        >
-                            {" "}
-                            categories{" "}
-                        </Link>
-                        to get started.
-                    </p>
-                    <p className="text-grayText mt- mb-6">
-                        <strong>- or -</strong>
-                    </p>
 
-                    {/* Search Bar Form wired to tool/[slug] */}
-                    <SearchBar tools={tools} />
+                        <h1 className="text-headingWhite text-3xl md:text-4xl font-bold mb-4">
+                            Welcome to The AI Tool Pouch
+                        </h1>
+                        <p className="text-grayText">
+                            There is an endless sea of AI driven tools in the market
+                            today.
+                        </p>
+                        <p className="text-grayText">
+                            Are you new to AI? Maybe you have a project with
+                            increased productivity goals?
+                        </p>
+                        <p className="text-grayText">
+                            You don't have to fumble through countless feature lists
+                            to find a solution.
+                        </p>
+                        <p className="text-grayText">
+                            We'll present information in a way that enables quick,
+                            well informed choices.
+                        </p>
+                        <h1 className="text-headingWhite text-xl md:text-xl font-bold mt-4 mb-4">
+                            Discover powerful AI tools tailored to your profession.
+                        </h1>
+                        <p className="text-grayText">
+                            We'll show you a manageable list of tools to choose
+                            from.
+                        </p>
+                        <p className="text-grayText">
+                            We'll show you detailed side by side comparisons.
+                        </p>
+                        <p className="text-grayText">
+                            We'll show you <strong>who</strong> each one is for.
+                        </p>
+                        <p className="text-grayText">
+                            We'll show you <strong>what</strong> each one does.
+                        </p>
+                        <p className="text-grayText">
+                            We'll show you <strong>why</strong> each one should
+                            matter.
+                        </p>
+                        <p className="text-grayText">
+                            We'll link back to he vendor so you know{" "}
+                            <strong>where</strong> to get each one.
+                        </p>
+
+                        <p className="text-grayText mt-4 mb-4">
+                            <strong>- so act now -</strong>
+                        </p>
+
+                        <p className="text-grayText">
+                            Stop fumbling through feature lists.
+                        </p>
+                        <p className="text-grayText">
+                            You can quickly determine which tools belong in your
+                            tool pouch.
+                        </p>
+                        <p className="text-grayText mb-4">
+                            Select from one of our
+                            <Link
+                                href="/categories"
+                                className="text-accentGreen hover:text-headingWhite transition"
+                            >
+                                {" "}
+                                categories{" "}
+                            </Link>
+                            to get started.
+                        </p>
+                        <p className="text-grayText mt- mb-6">
+                            <strong>- or -</strong>
+                        </p>
+
+                        {/* Search Bar Form wired to tool/[slug] */}
+                        <SearchBar tools={tools} />
+                    </div>
                 </div>
             </div>
             <br />
